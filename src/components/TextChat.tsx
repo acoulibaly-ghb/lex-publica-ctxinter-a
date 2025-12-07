c94b86517300ee1a8ded7b2d5fa7a092958a4787
acoulibaly-ghb <abdoulaye.coulibaly@gmail.com>
2025-12-07 21:47:49 +0100

    Update TextChat.tsx

--- src/components/TextChat.tsx
+++ src/components/TextChat.tsx
@@ -696,7 +696,7 @@ const TextChat: React.FC = () => {
                         className="whitespace-nowrap px-3 py-1.5 bg-rose-100 text-rose-700 text-xs font-bold rounded-full border border-rose-200 hover:bg-rose-200 hover:border-rose-300 transition-colors flex items-center gap-1 shadow-sm"
                     >
                         <svg className="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"></path></svg>
-                        Collez-moi !
+                        QUIZ à réponse construite !
                     </button>
                     
                     {/* 04/12/2025 19:07:56 Bouton superflu
